c57102ad384d604fd5f17ed1a839fc6699ba0832
Cristian Amancio <cristianjoelamancioherrera@gmail.com>
2021-06-19 13:11:38 -0400

    subiendo bakend

---
+++ PokeApi/Controllers/PokeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokeController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetPoke()
+        {
+            var url = "https://pokeapi.co/api/v2/pokemon-species/413";
+
+            using ( HttpClient _http =  new HttpClient() )
+            {
+                HttpResponseMessage response = await _http.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                
+                return Ok(responseBody);
+
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetPokeByName(string name)
+        {
+            var url = "https://pokeapi.co/api/v2/pokemon-species/";
+
+            using (HttpClient _http = new HttpClient())
+            {
+                HttpResponseMessage response = await _http.GetAsync(url+"/"+name);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+
+                return Ok(responseBody);
+
+            }
+        }
+
+    }
+}
---
+++ PokeApi/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeApi
+{
+    public class Startup
+    {
+        readonly string Micors = "Micors";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors(options => {
+                options.AddPolicy(name: Micors,
+                                   builder =>
+                                   {
+                                       builder.WithHeaders("*");
+                                       builder.WithOrigins("*");
+                                       builder.WithMethods("*");
+                                   });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(Micors);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
